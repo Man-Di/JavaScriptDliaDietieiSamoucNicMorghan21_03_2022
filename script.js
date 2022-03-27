33e25955b016ee395cbe33f89944613ae8047cf8
Man-Di <perfect8778@gmail.com>
2022-03-27 20:04:07 +0300

    27.03.2022-20:04-32 стр

---
+++ script.js
@@ -0,0 +1,24 @@
+var age = 34;
+console.log(age);
+
+
+
+var secondInAMinute = 60;
+var minutesInAHour = 60;
+var secondsInAnHour = secondInAMinute * minutesInAHour;
+secondsInAnHour;
+
+var hoursInADay = 24;
+var secondsInADay = secondsInAnHour * hoursInADay;
+secondsInADay;
+
+var daysInAYear = 365;
+var secondsInAYear = secondsInADay * daysInAYear;
+console.log(secondsInAYear);
+
+//возраст в секундах
+var ageInSeconds = age * secondsInAYear;
+console.log(ageInSeconds);
+
+// 1 072 224 000
+// 27.03.2022 - 32 стр
